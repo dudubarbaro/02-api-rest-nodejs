8de9e7785d2c5908a43db9480c2c35e54475db5f
Eduardo Barbaro <99221824+dudubarbaro@users.noreply.github.com>
2025-04-30 16:04:23 -0300

    adiciona rotas para manipulação de transações

--- db/migrations/20250305235451_create-transactions.ts
+++ db/migrations/20250305235451_create-transactions.ts
@@ -4,7 +4,6 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.createTable('transactions', (table) => {
         table.uuid('id').primary()
-<<<<<<< HEAD
         table.text('title').notNullable()
         table.decimal('amount', 10, 2).notNullable()
         table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
@@ -16,12 +15,3 @@ export async function down(knex: Knex): Promise<void> {
     await knex.schema.dropTable('transactions')
 }
 
-=======
-        table.uuid('title').notNullable()
-    })
-}
-
-export async function down(knex: Knex): Promise<void> {
-    await knex.schema.dropTable('transactions')
-}
->>>>>>> 0d6b1b7d10254eb2239c1fbfebdd71f4cd1e1a43
